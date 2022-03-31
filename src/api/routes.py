1455f2bf7d40a61978f41e652aa092727d9b3056
Keenan <kkan68@gmail.com>
2022-03-31 05:46:35 +0000

    endpoints being made

--- src/api/routes.py
+++ src/api/routes.py
@@ -67,7 +67,7 @@ def update_user():
 
 #Todo Endpoints
 @api.route('/todos', methods=['GET'])
-# @jwt_required()
+@jwt_required()
 def get_todos():
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
@@ -78,7 +78,7 @@ def get_todos():
     return jsonify(all_serialized_todos)
 
 @api.route('/todos', methods=['POST'])
-# @jwt_required()
+@jwt_required()
 def create_todos():
     task = request.json.get('task', None)
     stage = request.json.get('stage', None)
@@ -92,3 +92,47 @@ def create_todos():
     db.session.add(todos)
     db.session.commit()
     return jsonify(todos.serialize())
+
+@api.route('/todos', methods=['PUT'])
+@jwt_required()
+def update_todo():
+    current_user_id = get_jwt_identity()
+    todoid = request.json.get('id')
+    todo = Todos.query.filter_by(id=todoid, creator=current_user_id).first()
+    if todo is None:
+        return jsonify({"msg":"Item or User doesn't exist"}), 400
+    task = request.json.get('task')
+    stage = request.json.get('stage')
+    duedate = request.json.get('duedate')
+
+    if task is None or not task:
+        todo.task = todo.task
+    else:
+        todo.task = task
+
+    if stage is None or not stage:
+        todo.stage = todo.stage
+    else:
+        todo.stage = stage
+
+    if duedate is None or not duedate:
+        todo.duedate = todo.duedate
+    else:
+        todo.duedate = duedate
+
+    db.session.commit()
+    return jsonify(todo.serialize())
+
+@api.route('/todos', methods=['DELETE'])
+@jwt_required()
+def delete_todo():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    user_id = request.json.get('user')
+    todo_id_get = request.json.get('id')
+    target_todo = Todos.query.filter_by(creator=user_id, id=todo_id_get).first()
+    if target_todo is None: 
+        return jsonify({"msg": "Invalid todo"}), 400
+    db.session.delete(target_todo)
+    db.session.commit()
+    return jsonify({ "msg": "Todo Deleted"}), 200
